80d93088147119f72f9c66f372dffc9d24f05e56
Khaled Zaza <khaledzaza.cs@gmail.com>
2022-10-11 00:06:24 -0700

    created the Queue

--- src/myQueue/MyQueue.java
+++ src/myQueue/MyQueue.java
@@ -2,4 +2,70 @@ package myQueue;
 //using an array
 
 public class MyQueue {
+    int[] queue;
+    int rear;
+    int front;
+    int size; // total number of elements in queue
+
+    //constructor to create a queue
+    public MyQueue(int sizeOfArray){
+        queue = new int[sizeOfArray];
+        rear = -1;
+        front = -1;
+        size = 0;
+    }
+
+    //check if the queue is empty
+    public boolean isEmpty(){
+        boolean response = false;
+        if (size == 0){
+            response =true;
+
+        }
+        return response;
+    }
+
+
+
+
+    //add element to the queue, return true if the element is added
+    public boolean enqueue(int element){
+        boolean response =false;
+
+        if(rear != queue.length -1){
+            rear++;
+            queue[rear] = element;
+            size++;
+            response =true;
+        }
+        return response;
+    }
+
+
+    //remove a number
+    public int dequeue(){
+        int responce =0;
+        if (size != 0){
+            front ++;
+            responce = queue[front];
+            size--;
+        }
+        return responce;
+    }
+
+
+    //get the front element
+    public int peek(){
+        int response = 0;
+        if(!isEmpty()){
+            response = queue[front +1];
+        }
+        return response;
+
+    }
+    //get the size
+    public int getSize(){
+        return size;
+    }
+
 }
